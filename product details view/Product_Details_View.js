fee8e8485766f87204a1e8602797b5ce29c5d070
XotEmBotZ <xotembotzop@gmail.com>
2022-08-07 01:25:21 +0530

    Css fix for font weight and input fix in productDetails

--- product details view/Product_Details_View.js
+++ product details view/Product_Details_View.js
@@ -10,3 +10,31 @@ for (element of sideImgs){
 	console.log(element)
 	element.addEventListener('click',e=>changeImg(e.target.id))
 }
+
+// BookQtyBtn Manupulation
+
+let ele = document.getElementsByClassName("bookQuantityBtn");
+
+dataIncrease=(increaseElement,input)=>{
+    increaseElement.addEventListener('click',e=>{
+        console.log(input)
+        input.value=parseInt(input.value)+1
+    })
+}
+
+dataDecrease=(decreaseElement,input)=>{
+    decreaseElement.addEventListener('click',e=>{
+        console.log(input)
+        input.value=parseInt(input.value)-1
+    })
+}
+
+
+for (ele of ele){
+    let btns=ele.getElementsByTagName('button')
+    let inp=ele.getElementsByTagName('input')[0]
+    dataIncrease(btns[1],inp)
+    dataDecrease(btns[0],inp)
+    console.log(btns)
+    console.log(inp)
+}
