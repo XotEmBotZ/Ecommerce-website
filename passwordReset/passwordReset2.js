878b8b95fb87bc08fbb81cafeacac4095306181a
XotEmBotZ <xotembotzop@gmail.com>
2022-08-09 01:01:37 +0530

    Added password validation

---
+++ passwordReset/passwordReset2.js
@@ -0,0 +1,50 @@
+let password = document.getElementById("newpassword")
+let repeatPassword = document.getElementById("repeatpassword")
+
+let passowrdValidation = document.getElementById("passwordValidation")
+let passowrdValidationTemplate = document.getElementById("passwordValidationTemplate")
+
+let passwordRepeatValidation = document.getElementById("passwordRepeatValidation")
+let passwordRepeatValidationTemplate = document.getElementById("passowrdRepeatValidationTemplate")
+
+checkPassword = (passowrd) => {
+    passowrdValidation.innerHTML = ''
+    password.parentElement.classList.remove('correct')
+    let passowrdValidationClone = passowrdValidationTemplate.content.cloneNode(true)
+    
+    alltrue=true
+    
+    var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    if (passowrd.match(lowerCaseLetters) && passowrd.match(upperCaseLetters)) {
+        passowrdValidationClone.getElementById("lowerAndUpperCase").classList.add('fulfiled')
+        alltrue=alltrue&&true
+    }else{
+        alltrue=alltrue&&false
+    }
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (passowrd.match(numbers)) {
+        alltrue=alltrue&&true
+        passowrdValidationClone.getElementById("numberAndSymbol").classList.add('fulfiled')
+    }else{
+        alltrue=alltrue&&false
+    }
+    
+    // Validate length
+    if (passowrd.length >= 8) {
+        alltrue=alltrue&&true
+        console.log("Woring",passowrdValidationClone.getElementById("numberAndSymbol"))
+        passowrdValidationClone.getElementById("charLength").classList.add('fulfiled')
+    }else{
+        alltrue=alltrue&&false
+    }
+    passowrdValidation.appendChild(passowrdValidationClone)
+    if(alltrue){
+        passowrdValidation.innerHTML=''
+        password.parentElement.classList.add('correct')
+    }
+}
+password.addEventListener('keyup', e => {
+    checkPassword(password.value)
+})
