d1b09aa78e5c16d58aa6e4b6ce158cfb31fd777f
XotEmBotZ <xotembotzop@gmail.com>
2022-08-21 01:13:29 +0530

    Added responsiveness to product listing page

--- product listing page/Product_Listing_Page.js
+++ product listing page/Product_Listing_Page.js
@@ -2,3 +2,32 @@ let notification = document.getElementById("notification")
 notification.addEventListener('click',e=>{
     notification.classList.add("dNone")
 })
+
+
+let hamburgerDiv = document.getElementById("hamburgerMobile")
+let nav = document.getElementById("nav")
+hamburgerDiv.addEventListener('click',e=>{
+    if (nav.classList.contains('dNone')){
+        nav.classList.remove("dNone")
+        hamburgerDiv.classList.add("hamburgerOpened")
+    }else{
+        nav.classList.add("dNone")
+        hamburgerDiv.classList.remove("hamburgerOpened")
+    }
+})
+
+if (window.screen.width<=480){
+    nav.classList.add('dNone');
+}
+
+let mobileButtonOpener=document.getElementById("filterActiveBtnMobile")
+let mainFilter=document.getElementById("mainFilter")
+mobileButtonOpener.addEventListener('click',e=>{
+    if (mainFilter.getAttribute("data-active")=="true"){
+        mainFilter.setAttribute("data-active","false")
+        mobileButtonOpener.getElementsByTagName('i')[0].innerHTML="filter_list"
+    }else{
+        mobileButtonOpener.getElementsByTagName('i')[0].innerHTML="close";
+        mainFilter.setAttribute("data-active","true")
+    }
+})
